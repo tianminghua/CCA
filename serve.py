70bbcd42d7c2a910e2a0fa98f5ea6d65a1e2c0d1
Dezhao <lidezhao13@gmail.com>
2024-01-20 15:39:19 -0500

    first commit

---
+++ serve.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+import subprocess
+import socket
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def stress_cpu():
+    try:
+        # Start the CPU stress script in a non-blocking manner
+        subprocess.Popen(['python', 'stress_cpu.py'])
+        return 'CPU stress process started.', 200
+    except Exception as e:
+        return str(e), 500
+
+@app.route('/', methods=['GET'])
+def get_private_ip():
+    try:
+        hostname = socket.gethostname()
+        private_ip = socket.gethostbyname(hostname)
+        return private_ip
+    except Exception as e:
+        return str(e), 500
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8000)
